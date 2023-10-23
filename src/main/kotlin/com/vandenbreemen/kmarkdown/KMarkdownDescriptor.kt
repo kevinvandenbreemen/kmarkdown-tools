749f25fb008b6f07d189833a84197be260ae60ef
kevin <kevin.vandenbreemen@gmail.com>
2023-10-23 16:20:43 -0700

    Borrow existing sequential parser setup from supertype

--- src/main/kotlin/com/vandenbreemen/kmarkdown/KMarkdownDescriptor.kt
+++ src/main/kotlin/com/vandenbreemen/kmarkdown/KMarkdownDescriptor.kt
@@ -1,6 +1,11 @@
 package com.vandenbreemen.kmarkdown
 
+import org.intellij.markdown.MarkdownTokenTypes
 import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.parser.sequentialparsers.EmphasisLikeParser
+import org.intellij.markdown.parser.sequentialparsers.SequentialParser
+import org.intellij.markdown.parser.sequentialparsers.SequentialParserManager
+import org.intellij.markdown.parser.sequentialparsers.impl.*
 
 /**
  *
@@ -9,6 +14,17 @@ class KMarkdownDescriptor(useSafeLinks: Boolean = true,
                           absolutizeAnchorLinks: Boolean = false):
     CommonMarkFlavourDescriptor(useSafeLinks, absolutizeAnchorLinks) {
 
-
+    override val sequentialParserManager = object : SequentialParserManager() {
+        override fun getParserSequence(): List<SequentialParser> {
+            return listOf(
+                AutolinkParser(listOf(MarkdownTokenTypes.AUTOLINK)),
+                BacktickParser(),
+                ImageParser(),
+                InlineLinkParser(),
+                ReferenceLinkParser(),
+                EmphasisLikeParser(EmphStrongDelimiterParser())
+            )
+        }
+    }
 
 }
