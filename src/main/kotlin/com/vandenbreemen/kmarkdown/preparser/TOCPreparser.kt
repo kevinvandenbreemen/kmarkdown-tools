6340fcf1a326e90525136836707acdbac8ca45bd
kevin <kevin.vandenbreemen@gmail.com>
2023-10-26 14:11:19 -0700

    Incorporate testing done on the main wiki project

---
+++ src/main/kotlin/com/vandenbreemen/kmarkdown/preparser/TOCPreparser.kt
@@ -0,0 +1,43 @@
+package com.vandenbreemen.kmarkdown.preparser
+
+import java.util.*
+
+/**
+ * Table of contents preparsing.  This can be used as a standalone program
+ */
+class TOCPreparser {
+    fun parse(markdownContent: String): String {
+
+        val headersRegex = Regex("(?m)^#+\\s+(.*)$")
+
+        val headers = headersRegex.findAll(markdownContent)
+            .map {
+                it.groupValues[0] to it.groupValues[1] }
+            .toList()
+
+        val headerLinks = mutableMapOf<String, String>()
+
+        val tableOfContents = StringBuilder()
+
+        if (headers.isNotEmpty()) {
+            tableOfContents.append("Table of Contents\n")
+            for ((hashes, header) in headers) {
+                val indentation = "  ".repeat(hashes.count { it == '#' } - 1)
+                tableOfContents.append("$indentation- [${header.trim()}](#${
+                    header.lowercase(Locale.getDefault()).replace("\\s+".toRegex(), "-").also { link->
+                        headerLinks[hashes] = link
+                    }})\n")
+            }
+        }
+
+        var updatedMarkdown = markdownContent
+        headerLinks.entries.forEach { headerToLink->
+            updatedMarkdown = updatedMarkdown.replace(headerToLink.key, "<section id=\"${headerToLink.value}\">\n\n${headerToLink.key}\n</section>")
+        }
+
+        return StringBuilder(tableOfContents.toString()).append("\n\n").append(updatedMarkdown).toString()
+
+    }
+
+
+}
---
+++ src/test/kotlin/com/vandenbreemen/kmarkdown/preparser/TOCPreparserTest.kt
@@ -0,0 +1,28 @@
+package com.vandenbreemen.kmarkdown.preparser
+
+import org.amshove.kluent.shouldContain
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class TOCPreparserTest {
+
+    @Test
+    fun `should parse table of contents`() {
+
+        val markdown = """
+# Heading 1
+Hello world
+
+This is a test
+
+## Heading 2
+A sub-heading
+        """.trimIndent()
+
+        val parsed = TOCPreparser().parse(markdown)
+
+        parsed.shouldContain("section id=\"heading-2\"")
+
+    }
+
+}
